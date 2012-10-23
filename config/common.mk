fc77d00d68d2c70070f0c6370d05d0f6e3b10682
xxxifan <445947962@qq.com>
2012-10-23 20:34:55 +0800

    update
    Change-Id: Iea0c6fac232bfbf610abd76d3d022fb13dd6a55f

--- config/common.mk
+++ config/common.mk
@@ -194,7 +194,7 @@ TEAM_HEAD = ToxMod
     EXTRAVERSION :=
 
     TM_VERSION := $(PRODUCT_VERSION_MAJOR)-$(shell date -u +%Y%m%d)-$(BUILDTYPE)-$(CM_BUILD)$(CM_EXTRAVERSION)
-    VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(BUILD_ID)-$(shell date -u +%Y%m%d)-$(TEAM_BUILDER)
+    VERSION := $(TEAM_HEAD)-$(PLATFORM_VERSION)-$(BUILD_ID)-$(shell date -u +%Y%m%d)-$(TEAM_BUILDER)
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(TM_VERSION) \
-  ro.modversion=$(VERSION)/
+  ro.modversion=$(VERSION)
